fb7667641326e40912ff5ba3c1c0b0f2c8d7c422
Vladimir Petrigo <taenaru@gmail.com>
2019-08-08 12:42:19 +0300

    builder: Clean up

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,10 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::parse_macro_input;
+use syn::DeriveInput;
+use proc_macro2::Ident;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
