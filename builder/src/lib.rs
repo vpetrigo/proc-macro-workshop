56926a38396b834af0e2b4370116fcf6156eb3bd
Vladimir Petrigo <taenaru@gmail.com>
2019-08-09 11:51:16 +0300

    builder: Clean up

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -8,7 +8,15 @@ use proc_macro2::Ident;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
+    let struct_name: &Ident = &ast.ident;
+    let fields = get_fields(&ast);
+    let builder_fields = generate_builder_fields(fields);
+    let constructor_fields = generate_constructor_fields(fields);
+    let builder_fields_empty = generate_emtpy_fields(fields);
+    let struct_builder_name = get_builder_name(&struct_name);
+    let struct_builder = syn::Ident::new(&struct_builder_name, struct_name.span());
+    let methods = generate_builder_functions(fields);
 
     unimplemented!()
 }
