8ec467fa6328e938cd6fac0210dffeff5a00f9eb
Vladimir Petrigo <taenaru@gmail.com>
2019-08-22 00:41:14 +0300

    debug: Update dependencies to pass test 1

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,10 +1,11 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use syn::parse_macro_input;
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as syn::DeriveInput);
 
-    unimplemented!()
+    TokenStream::new()
 }
