f3db23ba7c1fa50423efbfdac442a6863fc53c0a
Vladimir Petrigo <taenaru@gmail.com>
2019-08-27 06:06:11 +0300

    debug: Add parse_quote dependency

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -2,7 +2,7 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::parse_macro_input;
+use syn::{parse_macro_input, parse_quote};
 
 #[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
